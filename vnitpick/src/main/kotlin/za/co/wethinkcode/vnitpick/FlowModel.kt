2cfefe0e0b89b3d115cf054ad72e0347fafebfa8
GeePaw Hill <geepawhill@geepawhill.org>
2025-01-25 16:03:44 -0500

    Correct layout of commits.

--- vnitpick/src/main/kotlin/za/co/wethinkcode/vnitpick/FlowModel.kt
+++ vnitpick/src/main/kotlin/za/co/wethinkcode/vnitpick/FlowModel.kt
@@ -1,14 +1,20 @@
 package za.co.wethinkcode.vnitpick
 
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import za.co.wethinkcode.core.parse.Base64Loader
 import za.co.wethinkcode.core.parse.LogCollater
+import za.co.wethinkcode.core.parse.LogShape
+import za.co.wethinkcode.core.parse.ShapeDesigner
 import za.co.wethinkcode.core.parse.YamlConverter
 import java.nio.file.Path
 
 class FlowModel {
 
     val raw = mutableStateListOf("")
+    val width = mutableStateOf(0)
+    val height = mutableStateOf(0)
+    val shapes = mutableStateListOf<LogShape>()
 
     fun load(path: Path) {
         val yamls = Base64Loader().load(path.resolve(".jltk"))
@@ -16,7 +22,11 @@ class FlowModel {
         yamls.forEach { yaml -> raw.add(yaml) }
         val entries = YamlConverter().convert(yamls)
         val commits = LogCollater().collate(entries)
-
+        val layout = ShapeDesigner().design(commits)
+        width.value = layout.width
+        height.value = layout.height
+        shapes.clear()
+        shapes.addAll(layout.shapes)
     }
 }
 
--- vnitpick/src/main/kotlin/za/co/wethinkcode/vnitpick/ProjectView.kt
+++ vnitpick/src/main/kotlin/za/co/wethinkcode/vnitpick/ProjectView.kt
@@ -1,5 +1,7 @@
 package za.co.wethinkcode.vnitpick
 
+import androidx.compose.foundation.BasicTooltipBox
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -10,20 +12,26 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberBasicTooltipState
+import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.PopupPositionProviderAtPosition
+import za.co.wethinkcode.core.parse.CommitShape
 
 @Composable
 fun ProjectView(model: ProjectsModel) {
@@ -91,9 +99,63 @@ fun ProjectPage(project: ProjectModel) {
 
 @Composable
 fun ProcessPage(model: FlowModel) {
-    LazyColumn(Modifier.fillMaxSize()) {
-        items(model.raw) { item ->
-            Text(item)
+    Box(Modifier.fillMaxSize().background(color = Color.LightGray)) {
+        Box(
+            Modifier.width((20 * model.width.value).dp)
+                .height((20 * model.height.value).dp)
+                .background(color = Color.Yellow)
+        ) {
+            model.shapes.forEach {
+                when (it) {
+                    is CommitShape -> DrawCommitShape(it, model.height.value)
+                }
+            }
         }
+    }
+}
+
+private val TempCommitShape = GenericShape { size, _ ->
+    moveTo(0f, size.height)
+    lineTo(0f, size.height - 20f)
+    lineTo(size.width - 20f, size.height - 20f)
+    lineTo(size.width - 20f, 0f)
+    lineTo(size.width, 0f)
+    lineTo(size.width, size.height)
+}
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
+@Composable
+fun DrawCommitShape(shape: CommitShape, totalHeight: Int) {
+    val tooltipState = rememberBasicTooltipState(isPersistent = true)
+    val offsetX = shape.x * 20
+    val offsetY = (totalHeight - shape.height) * 20
+    println("$offsetX,$offsetY")
+    println("${shape.x} ${shape.height}")
+    println("${shape.width},${shape.height}")
+    BasicTooltipBox(
+        modifier = Modifier.offset(offsetX.dp, offsetY.dp)
+            .clip(TempCommitShape),
+        positionProvider = PopupPositionProviderAtPosition(
+            Offset(0f, 0f),
+            true,
+            Offset(0f, 0f),
+            windowMarginPx = 5
+        ),
+        tooltip = {
+            Text("${shape.detail.timestamp}")
+        },
+        state = tooltipState
+    ) {
+        Box(
+            modifier = Modifier.width((shape.width * 20).dp)
+                .height((shape.height * 20).dp)
+//                .offset(offsetX.dp, offsetY.dp)
+                .clip(TempCommitShape)
+                .clickable {
+                    println("Clicked")
+                }
+                .background(Color.Red)
+                .border(1.dp, Color.Blue, shape = TempCommitShape)
+        )
     }
 }

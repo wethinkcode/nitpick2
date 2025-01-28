a1e3980e1898725934c6cce486716427bddc32f0
GeePaw Hill <geepawhill@geepawhill.org>
2025-01-28 16:38:09 -0500

    Pan and Zoom semi-operational.

--- vnitpick/src/main/kotlin/za/co/wethinkcode/vnitpick/FlowView.kt
+++ vnitpick/src/main/kotlin/za/co/wethinkcode/vnitpick/FlowView.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -16,6 +18,7 @@ import androidx.compose.foundation.rememberBasicTooltipState
 import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,34 +35,40 @@ import za.co.wethinkcode.core.flow.TestStatus
 
 @Composable
 fun FlowPage(model: FlowModel) {
-    Box(Modifier.fillMaxSize().background(color = Color.LightGray)) {
-        if (model.isJltk.value) {
-            Box(
-                Modifier.width((20 * model.width.value).dp).height((20 * model.height.value).dp)
-                    .background(color = Color.LightGray)
+    if (model.isJltk.value == false) {
+        AdviceBox(
+            listOf(
+                "This is not a jltk project.\n", "Please open a jltk project."
+            )
+        )
+    } else {
+        PanAndZoom {
+            Column(
+                Modifier.fillMaxSize().background(Color.White),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                model.shapes.forEach {
-                    when (it) {
-                        is CommitShape -> FlowCommit(it, model.height.value) {
-                            model.flowClick(it)
-                        }
+                Box(
+                    Modifier.width((20 * model.width.value).dp).height((20 * model.height.value).dp)
+                        .background(color = Color.LightGray)
+                ) {
+                    model.shapes.forEach {
+                        when (it) {
+                            is CommitShape -> FlowCommit(it, model.height.value) {
+                                model.flowClick(it)
+                            }
 
-                        is BarShape -> FlowBar(it, model.height.value) {
-                            model.flowClick(it)
-                        }
+                            is BarShape -> FlowBar(it, model.height.value) {
+                                model.flowClick(it)
+                            }
 
-                        is TestShape -> FlowTest(it, model.height.value) {
-                            model.flowClick(it)
+                            is TestShape -> FlowTest(it, model.height.value) {
+                                model.flowClick(it)
+                            }
                         }
                     }
                 }
             }
-        } else {
-            AdviceBox(
-                listOf(
-                    "This is not a jltk project.\n", "Please open a jltk project."
-                )
-            )
         }
     }
 }
@@ -118,6 +127,7 @@ fun FlowTest(shape: TestShape, totalHeight: Int, onClick: () -> Unit) {
         }.background(background).border(1.dp, Color.Black, shape = RectangleShape))
     }
 }
+
 
 @Composable
 fun FlowTip(offsetX: Int, offsetY: Int, isEll: Boolean, tip: String, content: @Composable () -> Unit) {
---
+++ vnitpick/src/main/kotlin/za/co/wethinkcode/vnitpick/PanAndZoom.kt
@@ -0,0 +1,61 @@
+package za.co.wethinkcode.vnitpick
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
+
+@Composable
+fun PanAndZoom(content: @Composable () -> Unit) {
+    var scale by remember { mutableStateOf(1f) }
+    var rotation by remember { mutableStateOf(0f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+        scale *= zoomChange
+        rotation += rotationChange
+        offset += offsetChange
+    }
+    Column(Modifier.fillMaxSize()) {
+        Row(Modifier.fillMaxWidth()) {
+            Button({ scale *= 1.1f }) { Text("Bigger") }
+            Button({ scale *= .9f }) { Text("Smaller") }
+        }
+        Surface(
+            Modifier
+                // apply other transformations like rotation and zoom
+                // on the pizza slice emoji
+                .clip(RectangleShape)
+                .graphicsLayer(
+                    scaleX = scale,
+                    scaleY = scale,
+                    rotationZ = rotation,
+                    translationX = offset.x,
+                    translationY = offset.y
+                )
+                // add transformable to listen to multitouch transformation events
+                // after offset
+                .transformable(state = state)
+                .background(Color.Blue)
+                .fillMaxSize()
+        ) {
+            content()
+        }
+    }
+}

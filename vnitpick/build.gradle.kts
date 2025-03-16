d3bf101d40220a8d0c88919a5a3330f172369a2b
GeePaw Hill <geepawhill@geepawhill.org>
2025-03-16 13:27:37 -0400

    Fix flow dependency to 0.1.0

--- vnitpick/build.gradle.kts
+++ vnitpick/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.7.6")
     implementation("br.com.devsrsouza.compose.icons:font-awesome:1.1.0")
     implementation("dev.hydraulic.conveyor:conveyor-control:1.1")
-    implementation("za.co.wethinkcode:flow:0.0.2")
+    implementation("za.co.wethinkcode:flow:0.1.0")
     implementation(project(":core"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
     testImplementation("org.assertj:assertj-core:3.24.2")

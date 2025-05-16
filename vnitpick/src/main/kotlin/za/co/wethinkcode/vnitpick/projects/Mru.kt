6b66eead69e8dd7296757278403d6cfd57e8a4db
GeePaw Hill <geepawhill@geepawhill.org>
2025-05-16 12:36:21 -0400

    Basic MRU added and tested.

--- vnitpick/src/main/kotlin/za/co/wethinkcode/vnitpick/projects/Mru.kt
+++ vnitpick/src/main/kotlin/za/co/wethinkcode/vnitpick/projects/Mru.kt
@@ -1,19 +1,55 @@
 package za.co.wethinkcode.vnitpick.projects
 
+import java.nio.charset.Charset
 import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import kotlin.io.path.*
 
-class Mru(val from: Path = Path.of("~/.lms")) {
+class Mru(val from: Path = Path.of("~/.lms"), val keep: Int = 10) {
 
-    private val paths = mutableSetOf<Path>()
+    private val paths = mutableListOf<Path>()
 
-    val items get() = paths.toList()
+    val items get() = if (paths.isEmpty()) listOf(from) else paths.reversed()
 
     init {
-
+        load()
     }
 
     fun add(path: Path) {
+        if (paths.contains(path)) {
+            paths.remove(path)
+        } else {
+            if (paths.size >= keep) {
+                paths.remove(paths.first())
+            }
+        }
+        paths.add(path)
+        save()
+    }
 
+    private fun save() {
+        from.createDirectories()
+        val asStrings = paths.map { it.toAbsolutePath().toString() }
+        from.resolve(MRU_FILE).writeLines(
+            asStrings,
+            Charset.defaultCharset(),
+            StandardOpenOption.WRITE,
+            StandardOpenOption.CREATE,
+            StandardOpenOption.TRUNCATE_EXISTING
+        )
+    }
+
+    fun load() {
+        from.createDirectories()
+        val path = from.resolve(MRU_FILE)
+        if (path.exists()) {
+            val lines = path.readLines()
+            lines.forEach { paths.add(Path.of(it)) }
+        }
+    }
+
+    companion object {
+        const val MRU_FILE = "mru.txt"
     }
 
 }
--- vnitpick/src/test/kotlin/za/co/wethinkcode/vnitpick/projects/MruTest.kt
+++ vnitpick/src/test/kotlin/za/co/wethinkcode/vnitpick/projects/MruTest.kt
@@ -1,15 +1,84 @@
 package za.co.wethinkcode.vnitpick.projects
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import za.co.wethinkcode.core.TestFolder
+import za.co.wethinkcode.vnitpick.projects.Mru.Companion.MRU_FILE
+import kotlin.io.path.exists
 
 class MruTest {
 
     val folder = TestFolder()
-    val mru = Mru(folder.root)
+    val mru = Mru(folder.root, 3)
 
     @Test
-    fun testMru() {
-        println(mru.items)
+    fun emptyMruOnlyContainsHome() {
+        assertThat(mru.items).containsExactly(folder.root)
+        folder.delete()
+    }
+
+    @Test
+    fun addAdds() {
+        val first = folder.root.resolve("path1")
+        mru.add(first)
+        assertThat(mru.items).containsExactly(first)
+        folder.delete()
+    }
+
+
+    @Test
+    fun listIsLifo() {
+        val first = folder.root.resolve("path1")
+        mru.add(first)
+        val second = folder.root.resolve("path2")
+        mru.add(second)
+        assertThat(mru.items).containsExactly(second, first)
+        folder.delete()
+    }
+
+    @Test
+    fun repositionsDuplicates() {
+        val first = folder.root.resolve("path1")
+        mru.add(first)
+        val second = folder.root.resolve("path2")
+        mru.add(second)
+        mru.add(first)
+        assertThat(mru.items).containsExactly(first, second)
+        folder.delete()
+    }
+
+    @Test
+    fun respectsCacheSize() {
+        val first = folder.root.resolve("path1")
+        mru.add(first)
+        val second = folder.root.resolve("path2")
+        mru.add(second)
+        val third = folder.root.resolve("path3")
+        mru.add(third)
+        val fourth = folder.root.resolve("path4")
+        mru.add(fourth)
+        assertThat(mru.items).containsExactly(fourth, third, second)
+        folder.delete()
+    }
+
+    @Test
+    fun addSaves() {
+        val first = folder.root.resolve("path1")
+        mru.add(first)
+        assertThat(folder.root.resolve(MRU_FILE).exists()).isTrue()
+        folder.delete()
+    }
+
+    @Test
+    fun loadsOnConstruction() {
+        val first = folder.root.resolve("path1")
+        mru.add(first)
+        val second = folder.root.resolve("path2")
+        mru.add(second)
+        val third = folder.root.resolve("path3")
+        mru.add(third)
+        val newMru = Mru(folder.root, 3)
+        assertThat(newMru.items).containsExactly(third, second, first)
+        folder.delete()
     }
 }
